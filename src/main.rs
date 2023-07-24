c99c4a3f04d371ed120cc53f803e0175ade82cb8
Danial Saleemi <salimicse@gmail.com>
2023-07-24 11:21:27 -0700

    Completed Number guess game

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Secret number is: {secret_number}", );
+    loop{
     println!("Please input your guess");
 
     let mut guess = String::new();
-    io::stdin();
-        .read_line(&mut guess);
+    io::stdin()
+        .read_line(&mut guess)
         .expect("Failed to read line");
-    println!("You guesssed: {guess}");
-    
+
+    let guess:u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => { 
+            println!("Invalid input. Enter a positive integer");
+            continue;
+        }
+    };    
+    println!("You guesssed: {guess}");  
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        }
+    }
+}
 }
